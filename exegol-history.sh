bc6c40e4c326e4c2b856871dd4f339e95c2b9819
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2025-03-07 18:46:40 +0100

    added bash wrapper and CI

---
+++ exegol-history.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+PYTHON_SCRIPT="$SCRIPT_DIR/exegol-history.py"
+
+case "$1" in
+    export)
+        if [ -z "$2" ]; then
+            exit 1
+        fi
+        VARS=$("$PYTHON_SCRIPT" export "$@")
+        if [ $? -eq 0 ]; then
+            if [ -n "$VARS" ]; then
+                eval "$VARS"
+                NUM_VARS=$(echo "$VARS" | wc -l)
+                echo "[+] Exported $NUM_VARS variables."
+            else
+                echo "[-] No variables to export."
+            fi
+        fi
+        ;;
+    env)
+        "$PYTHON_SCRIPT" env "$@"
+        ;;
+    *)
+        "$PYTHON_SCRIPT" "$@"
+        ;;
+esac 
