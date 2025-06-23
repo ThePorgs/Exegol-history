42f62f7adae559ef8bbe40918a736cf346e4f22e
lap1nou <lapinousexy@gmail.com>
2025-06-23 18:44:02 +0200

    test: Fixed a Windows test 3

--- exegol_history/tests/tui_tests/test_set_credential_tui.py
+++ exegol_history/tests/tui_tests/test_set_credential_tui.py
@@ -22,8 +22,6 @@ from exegol_history.tui.widgets.credential_form import (
     ID_USERNAME_INPUT,
 )
 
-
-@pytest.mark.skipif(sys.platform.startswith("win"), reason="require Linux")
 @pytest.mark.asyncio
 async def test_set_credential_without_selecting(
     open_keepass: PyKeePass, load_mock_config: dict[str, Any]
@@ -83,100 +81,9 @@ async def test_set_credential_only_username_linux(
     assert envs.strip() == ""
 
 
-@pytest.mark.skipif(sys.platform.startswith("lin"), reason="require Windows")
-@pytest.mark.asyncio
-async def test_set_credential_only_username_windows(
-    open_keepass: PyKeePass, load_mock_config: dict[str, Any]
-):
-    kp = open_keepass
-    app = DbCredsApp(load_mock_config, kp)
-    add_credential_keybind = load_mock_config["keybindings"]["add_credential"]
-
-    async with app.run_test() as pilot:
-        await pilot.press(add_credential_keybind)
-        await select_input_and_enter_text(
-            pilot, f"#{ID_USERNAME_INPUT}", USERNAME_TEST_VALUE
-        )
-        await pilot.click(f"#{ID_CONFIRM_BUTTON}")
-        await pilot.press(Keys.Enter)
-
-        write_credential_in_profile(
-            Credential(*pilot.app.return_value), load_mock_config
-        )
-
-    command_output = subprocess.run(
-        [
-            "powershell",
-            "-Command",
-            f". {load_mock_config['paths']['profile_sh_path']}; if ($?) {{ echo ${CREDS_VARIABLES[0]} }}",
-        ],
-        stdout=subprocess.PIPE,
-    )
-    envs = command_output.stdout.decode("utf8")
-
-    assert USERNAME_TEST_VALUE in envs
-
-    command_output = subprocess.run(
-        [
-            "powershell",
-            "-Command",
-            f". {load_mock_config['paths']['profile_sh_path']}; if ($?) {{ echo ${CREDS_VARIABLES[1]} ${CREDS_VARIABLES[2]} ${CREDS_VARIABLES[3]} }}",
-        ],
-        stdout=subprocess.PIPE,
-    )
-    envs = command_output.stdout.decode("utf8")
-
-    assert envs.strip() == ""
-
-# @pytest.mark.skipif(sys.platform.startswith("lin"), reason="require Windows")
-# @pytest.mark.asyncio
-# async def test_set_credential_only_username_windows(
-#    open_keepass: PyKeePass, load_mock_config: dict[str, Any]
-# ):
-#    kp = open_keepass
-# app = DbCredsApp(load_mock_config, kp)
-# add_credential_keybind = load_mock_config["keybindings"]["add_credential"]
-
-# async with app.run_test() as pilot:
-#    await pilot.press(add_credential_keybind)
-#    await select_input_and_enter_text(
-#        pilot, f"#{ID_USERNAME_INPUT}", USERNAME_TEST_VALUE
-#    )
-#    await pilot.click(f"#{ID_CONFIRM_BUTTON}")
-#    await pilot.press(Keys.Enter)
-#
-#    write_credential_in_profile(
-#        Credential(*pilot.app.return_value), load_mock_config
-#    )
-#
-# command_output = subprocess.run(
-#    [
-#        "bash",
-#        "-c",
-#        f"source {load_mock_config['paths']['profile_sh_path']} && echo ${CREDS_VARIABLES[0]}",
-#    ],
-#    stdout=subprocess.PIPE,
-# )
-# envs = command_output.stdout.decode("utf8")
-#
-# assert USERNAME_TEST_VALUE in envs
-#
-# command_output = subprocess.run(
-#    [
-#        "bash",
-#        "-c",
-#        f"source {load_mock_config['paths']['profile_sh_path']} && echo ${CREDS_VARIABLES[1]} ${CREDS_VARIABLES[2]} ${CREDS_VARIABLES[3]}",
-#    ],
-#    stdout=subprocess.PIPE,
-# )
-# envs = command_output.stdout.decode("utf8")
-#
-# assert envs.strip() == ""
-
-
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="require Linux")
 @pytest.mark.asyncio
-async def test_set_credential_half(
+async def test_set_credential_half_linux(
     open_keepass: PyKeePass, load_mock_config: dict[str, Any]
 ):
     kp = open_keepass
@@ -226,7 +133,7 @@ async def test_set_credential_half(
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="require Linux")
 @pytest.mark.asyncio
-async def test_set_credential_full(
+async def test_set_credential_full_linux(
     open_keepass: PyKeePass, load_mock_config: dict[str, Any]
 ):
     kp = open_keepass
@@ -257,6 +164,136 @@ async def test_set_credential_full(
             "bash",
             "-c",
             f"source {load_mock_config['paths']['profile_sh_path']} && echo ${CREDS_VARIABLES[0]} ${CREDS_VARIABLES[1]} ${CREDS_VARIABLES[2]} ${CREDS_VARIABLES[3]}",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    envs = command_output.stdout.decode("utf8")
+
+    assert USERNAME_TEST_VALUE in envs
+    assert PASSWORD_TEST_VALUE in envs
+    assert HASH_TEST_VALUE in envs
+    assert DOMAIN_TEST_VALUE in envs
+
+
+@pytest.mark.skipif(sys.platform.startswith("lin"), reason="require Windows")
+@pytest.mark.asyncio
+async def test_set_credential_only_username_windows(
+    open_keepass: PyKeePass, load_mock_config: dict[str, Any]
+):
+    kp = open_keepass
+    app = DbCredsApp(load_mock_config, kp)
+    add_credential_keybind = load_mock_config["keybindings"]["add_credential"]
+
+    async with app.run_test() as pilot:
+        await pilot.press(add_credential_keybind)
+        await select_input_and_enter_text(
+            pilot, f"#{ID_USERNAME_INPUT}", USERNAME_TEST_VALUE
+        )
+        await pilot.click(f"#{ID_CONFIRM_BUTTON}")
+        await pilot.press(Keys.Enter)
+
+        write_credential_in_profile(
+            Credential(*pilot.app.return_value), load_mock_config
+        )
+
+    command_output = subprocess.run(
+        [
+            "powershell",
+            "-Command",
+            f". {load_mock_config['paths']['profile_sh_path']}; if ($?) {{ echo ${CREDS_VARIABLES[0]} }}",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    envs = command_output.stdout.decode("utf8")
+
+    assert USERNAME_TEST_VALUE in envs
+
+    command_output = subprocess.run(
+        [
+            "powershell",
+            "-Command",
+            f". {load_mock_config['paths']['profile_sh_path']}; if ($?) {{ echo ${CREDS_VARIABLES[1]} ${CREDS_VARIABLES[2]} ${CREDS_VARIABLES[3]} }}",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    envs = command_output.stdout.decode("utf8")
+
+    assert envs.strip() == ""
+
+
+@pytest.mark.skipif(sys.platform.startswith("lin"), reason="require Windows")
+@pytest.mark.asyncio
+async def test_set_credential_half_windows(
+    open_keepass: PyKeePass, load_mock_config: dict[str, Any]
+):
+    kp = open_keepass
+    app = DbCredsApp(load_mock_config, kp)
+    add_credential_keybind = load_mock_config["keybindings"]["add_credential"]
+
+    async with app.run_test() as pilot:
+        await pilot.press(add_credential_keybind)
+        await select_input_and_enter_text(
+            pilot, f"#{ID_USERNAME_INPUT}", USERNAME_TEST_VALUE
+        )
+        await pilot.click(f"#{ID_CONFIRM_BUTTON}")
+        await pilot.press(Keys.Enter)
+
+        write_credential_in_profile(
+            Credential(*pilot.app.return_value), load_mock_config
+        )
+
+    command_output = subprocess.run(
+        [
+            "powershell",
+            "-Command",
+            f". {load_mock_config['paths']['profile_sh_path']}; if ($?) {{ echo ${CREDS_VARIABLES[0]} ${CREDS_VARIABLES[1]} }}",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    envs = command_output.stdout.decode("utf8")
+
+    assert USERNAME_TEST_VALUE in envs
+    assert PASSWORD_TEST_VALUE in envs
+
+    command_output = subprocess.run(
+        [
+            "powershell",
+            "-Command",
+            f". {load_mock_config['paths']['profile_sh_path']}; if ($?) {{ echo ${CREDS_VARIABLES[2]} ${CREDS_VARIABLES[3]} }}",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    envs = command_output.stdout.decode("utf8")
+
+    assert envs.strip() == ""
+
+
+@pytest.mark.skipif(sys.platform.startswith("lin"), reason="require Windows")
+@pytest.mark.asyncio
+async def test_set_credential_full_windows(
+    open_keepass: PyKeePass, load_mock_config: dict[str, Any]
+):
+    kp = open_keepass
+    app = DbCredsApp(load_mock_config, kp)
+    add_credential_keybind = load_mock_config["keybindings"]["add_credential"]
+
+    async with app.run_test() as pilot:
+        await pilot.press(add_credential_keybind)
+        await select_input_and_enter_text(
+            pilot, f"#{ID_USERNAME_INPUT}", USERNAME_TEST_VALUE
+        )
+        await pilot.click(f"#{ID_CONFIRM_BUTTON}")
+        await pilot.press(Keys.Enter)
+
+        write_credential_in_profile(
+            Credential(*pilot.app.return_value), load_mock_config
+        )
+
+    command_output = subprocess.run(
+        [
+            "powershell",
+            "-Command",
+            f". {load_mock_config['paths']['profile_sh_path']}; if ($?) {{ echo ${CREDS_VARIABLES[0]} ${CREDS_VARIABLES[1]} ${CREDS_VARIABLES[2]} ${CREDS_VARIABLES[3]} }}",
         ],
         stdout=subprocess.PIPE,
     )
